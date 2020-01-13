0c11b9acdd5bb38e525307848c29802b646c4052
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-01-13 13:20:30 +0200

    added some test javascript

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+console.log('This is it');
+
+var a = 1;
+var b = 2;
+var c = a + b;
+
+if (a < b) { 
+    //do the following things is a is lesser than b
+    console.log('a is lesser than b');
+    console.log(a + ' is lesser than ' + b);
+} else {
+    //do the following things otherwise
+    console.log('a is not greater than b');
+
+
+}
+
+
+
+//console.error(c);
