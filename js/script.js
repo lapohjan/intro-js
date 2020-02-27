f9abda62e90c523393456e9801c712b004588aae
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-02-27 10:20:00 +0200

    random changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
-var HOURHAND = document.getElementsById('hour');
-var MINUTEHAND = document.querySelector('.minute');
-var SECONDHAND = document.getElementsByName('second');
+var hourHand = document.getElementById("hour");
+var minuteHand = document.getElementById("minute");
+var secondHand = document.getElementById("second");
+
+function moveThoseArms() {
+  var date = new Date();
+  var hour = date.getHours() % 12;
+  var minute = date.getMinutes();
+  var second = date.getSeconds();
+
+  var hourDeg = hour * 30 + 0.5 * minute;
+  var minuteDeg = minute * 6 + 0.1 * second;
+  var secondDeg = second * 6;
+
+  hourHand.style.transform = "rotate(" + hourDeg + "deg)";
+  minuteHand.style.transform = "rotate(" + minuteDeg + "deg)";
+  secondHand.style.transform = "rotate(" + secondDeg + "deg)";
+}
+window.addEventListener("load", function() {
+  this.setInterval(moveThoseArms, 1000);
+});
+
+moveThoseArms();
